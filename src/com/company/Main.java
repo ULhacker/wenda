949d94e1a5e58b162644eb82812b2b78e785d343
ULhacker <1562028157@qq.com>
2019-03-17 16:34:59 +0800

    hello git

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,9 +3,6 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-
-            System.out.println("helloworld!");
-
-        System.out.println("你好2");
+        System.out.print("hello git!");
     }
 }
